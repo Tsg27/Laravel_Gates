a0ec05cccfb8fe1729272c1c0b396d2691a57d92
Tiago Soares <tsgo27@hotmail.com>
2025-02-07 17:32:01 -0300

    First Commit

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProdutoController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\Controller;
 use App\Http\Controllers\PostController;
